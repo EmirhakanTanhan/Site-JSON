dc74d54f1fb33646c26afcad7506fff62c3678b9
Emirhakan Tanhan <emirhakan.tanhan@gmail.com>
2021-06-18 11:44:06 +0300

    imp

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+import cors from 'cors';
+
 const express = require('express');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 const {products} = require('./data.json');
 
